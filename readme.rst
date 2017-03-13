14e6452a7ec9abec6d3c89bad3def4c4026095ce
Soo Rho <soobinrho@gmail.com>
2017-03-13 12:43:13 +0400

    small

---
+++ readme.rst
@@ -0,0 +1,11 @@
+Absconbest Payroll/Time Management
+==================================
+1. The xlsx file works well with both MS and Libreoffice. eg)
+   >>> libreoffice ~/Desktop/absconbest_payroll/absconbest_payroll.xlsx
+
+2. The program adds a command that generates a report and graph.
+   >>> generateAbsconbest
+
+The report is saved at
+   >>> "~/Desktop/absconbest_payroll/output/
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if not os.path.exists(dir_home):
 setup(
     name='absconbest_payroll',
     version='0.1b1',
-    description='Manages payroll and time',
+    description='Payroll and time management',
     url='https://github.com/soorho/absconbest_payroll',
     author='Soooo',
     author_email='soobinrho@gmail.com',
@@ -23,9 +23,20 @@ setup(
          ['absconbest_payroll/absconbest_payroll.xlsx', 'absconbest_payroll/read_cover_page_please.txt'])
     ],
     install_requires=[
+        'python3-dev',
         'pylatex',
         'plotly',
         'pandas',
     ],
-    zip_safe=False
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Office/Business :: Office Suites",
+        "Topic :: Office/Business :: Scheduling",
+    ],
 )
