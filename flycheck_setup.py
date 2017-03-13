68a176eac38546afa6254002dce33a5ee1bd7cd4
Soo Rho <soobinrho@gmail.com>
2017-03-13 14:07:52 +0400

    small

--- flycheck_setup.py
+++ flycheck_setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-dir_home=os.path.expanduser('~/Desktop/absconbest_payroll/')
+dir_home=os.path.expanduser("~/Desktop/absconbest_payroll/")
 if not os.path.exists(dir_home):
     os.makedirs(dir_home)
 
@@ -16,39 +16,27 @@ setup(
     packages=['absconbest_payroll'],
     include_package_data=True,
     scripts=['absconbest_payroll/generateAbsconbest'],
-
-    package_data={
-        'absconbest_payroll': ['logo.png'],
-    },
+    package_dir={'absconbest_payroll': 'absconbest_payroll'},
+    package_data={'absconbest_payroll': ['logo.png']},
     data_files=[
-        (
-            dir_home,
-            ['absconbest_payroll/absconbest_payroll.xlsx'],
-        )
+        (dir_home,
+         ['absconbest_payroll/absconbest_payroll.xlsx', 'absconbest_payroll/read_cover_page_please.txt'])
     ],
-
     install_requires=[
         'pandas',
         'pylatex',
         'plotly',
-        'numpy',
-        'python-dateutil',
-        'pytz',
-        'numexpr',
-        'bottleneck',
-        'matplotlib',
+        
     ],
-
-    zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Linguistic',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Office/Business :: Office Suites',
-        'Topic :: Office/Business :: Scheduling',
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Office/Business :: Office Suites",
+        "Topic :: Office/Business :: Scheduling",
     ],
 )
